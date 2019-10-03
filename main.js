8e81cd92c75abb795507af4192c0aee6669019cc
TaylorAkin <taylorakin.ky@gmail.com>
2019-10-03 12:32:44 -0400

    move tile function

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
+// newindex = [1, 2, 14, 5, 6, 3, 11, 0, 7, 9, 12, 4, 8, 10, 13, 15];
+
+var newindex;
+
+var newArr = [];
 
 var tileArray = []
 
-console.log(tileArray);
+var tileObjectArray = []
+
+// console.log(tileArray);
+
+
+class tileObject {
+    constructor(location, id){
+        this.id = id;
+        this.location = location;
+
+    }
+}
+
+
+
+
 
 function createpuzzle() {
 
@@ -15,8 +35,8 @@ function createpuzzle() {
     puzzlerow.className = 'row h-100';
 
 
-    var i = 1;
-    while (i < 17) {
+    var i = 0;
+    while (i < 16) {
 
         var puzzletiles = document.createElement('div');
         puzzletiles.addEventListener('click', moveTile);
@@ -26,14 +46,19 @@ function createpuzzle() {
         puzzlerow.appendChild(puzzletiles);
         puzzletiles.innerHTML = i;
 
+        var tile = new tileObject(i,tileArray[i])
+        tileObjectArray.push(tile);
+
         i++
-        console.log(puzzletiles.id);
+        // console.log(puzzletiles.id);
 
     }
 
+    // console.log(tileObjectArray);
+
     var buttonsContainer = document.createElement("div");
     buttonsContainer.id = 'buttonsContainerid';
-    puzzlecontainer.className = 'container h-100 w-50';
+    puzzlecontainer.className = 'container h-100 w-5 0';
 
     var buttonsRow = document.createElement("div");
     buttonsRow.id = 'buttonsRowId';
@@ -69,16 +94,85 @@ function createpuzzle() {
 }
 
 
+function blankTile(locat){
+    for(var i = 0; i < tileObjectArray.length; i++){
 
-function moveTile() {
+        if(tileObjectArray[i].location == locat){
+                return i;
+            
+        }
 
+    }
+}
+
+
+function moveTile(e) {
+
+    let clickedId = blankTile(this.id);
+    //location 0
+    var new_location = tileObjectArray[0].location;
+
+    //changing id's location placement. moving the 0 location to where you clicked
+    tileObjectArray[0].location = tileObjectArray[clickedId].location;
+    console.log(`new blank tile location: ${tileObjectArray[0].location}`)
+
+    tileObjectArray[clickedId].location = new_location;
+    console.log(`new clicked tile location: ${tileObjectArray[clickedId].location}`)
+
+
+    for (let i = 0; i < 16; i++) {
+        document.getElementById(`${tileObjectArray[i].location}`).innerHTML = i;
+    }
+
+    console.log(tileObjectArray);
 
 
 
 }
+
+
+
+
 //randomize puzzles id
+function getRandomInt(min, max, arr) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+
+
+
 function randomizer() {
 
+    var newArr = [];
+
+    for (var i = 0; i < tileArray.length; i++) {
+        var r = getRandomInt(0, 16, tileArray);
+
+        if (newArr.includes(r) == false) {
+
+            newArr.push(r);
+            document.getElementById(i).innerHTML = r;
+            // console.log('lalala');
+            tileObjectArray[i].id = r;
+
+        }
+        else {
+            i--;
+        }
+    }
+
+    tileArray = newArr;
+
+    // keeping ranomizer to != win case
+    if (tileArray[0] == 0) {
+
+        console.log('ta');
+        randomizer();
+
+    }
+console.log(tileObjectArray);
 
 }
 
